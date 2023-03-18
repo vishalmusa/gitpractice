6381f5b1b211cf89b51be6aaaa342bcf42d2499e
VishalMusale <vishalmusale1998@gmail.com>
2023-03-18 16:23:11 +0000

    2nd changes

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 Public static void main(String args[])
 {
-System.out.println(“hello this is features-101 branch”);
+System.out.println(“hello this is features-101 branch-2”);
 }
 }
 
